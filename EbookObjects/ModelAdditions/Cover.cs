95026ac5840f04abcafe23384174447b57f81576
Trevor Prinn <trev@babbacom.com>
2015-11-18 16:02:54 +0000

    Added some cover handling.

--- EbookObjects/ModelAdditions/Cover.cs
+++ EbookObjects/ModelAdditions/Cover.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 namespace EbookObjects.Models {
     partial class Cover {
 
+        private static byte[] _emptyThumbnail;
+
         // These properties returning Bitmaps are mainly for debugging, so the covers can be displayed in LinqPad.
 
         public Bitmap CoverPic => getBitmap(Picture);
@@ -44,5 +46,31 @@ namespace EbookObjects.Models {
             return cover;
         }
 
+        public static byte[] EmptyThumbnail {
+            get {
+                const int h = 100;
+                const int w = 70;
+                if (_emptyThumbnail == null) {
+                    using (var b = new Bitmap(w, h))
+                    using (var g = Graphics.FromImage(b)) {
+                        g.FillRectangle(Brushes.LightGray, 0, 0, w, h);
+                        g.DrawLine(Pens.Red, 0, 0, w, h);
+                        g.DrawLine(Pens.Red, 0, h, w, 0);
+                        ImageCodecInfo encoder = ImageCodecInfo.GetImageEncoders().Single(e => e.FormatID == ImageFormat.Jpeg.Guid);
+                        var encoderParams = new EncoderParameters(1);
+                        encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 75L);
+                        using (var m = new MemoryStream()) {
+                            b.Save(m, encoder, encoderParams);
+                            _emptyThumbnail = m.ToArray();
+                        }
+                    }
+                }
+                return _emptyThumbnail;
+            }
+        }
+
+        public static byte[] EmptyCover => EmptyThumbnail;
+
+        public string ImageContentType => "image/jpeg";
     }
 }
---
+++ EbookSite/Controllers/BookImageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EbookObjects.Models;
+
+namespace EbookSite.Controllers
+{
+    public class BookImageController : Controller
+    {
+        // GET: Image
+        public ActionResult ShowThumbnail(int bookId)
+        {
+            using (var db = new EbooksContext()) {
+                var thumbnail = db.Books.Single(b => b.BookId == bookId).Cover?.Thumbnail ?? Cover.EmptyThumbnail;
+                return new FileStreamResult(new MemoryStream(thumbnail), "image/jpeg");
+            }
+        }
+
+        public ActionResult ShowCover(int bookId) {
+            using (var db = new EbooksContext()) {
+                var cover = db.Books.Single(b => b.BookId == bookId).Cover;
+                var pic = cover?.Picture ?? Cover.EmptyCover;
+                var type = cover?.ImageContentType ?? "image/jpeg";
+                return new FileStreamResult(new MemoryStream(pic), type);
+            }
+        }
+    }
+}
